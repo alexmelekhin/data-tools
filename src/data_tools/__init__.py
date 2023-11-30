2496fae00d3284f986c22838bcfddfbc3de067c7
Alexander Melekhin <amelekhin96@gmail.com>
2023-11-30 22:11:43 +0300

    init commit

---
+++ src/data_tools/__init__.py
@@ -0,0 +1,8 @@
+"""Dataset processing tools package."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
